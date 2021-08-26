8b5c8605464f83c95e182dde9e48841c6fe5e2e5
Zhang Zibin <809309968@qq.com>
2021-08-26 23:53:05 +0800

    init log

--- log/LOG4J_STUDY/src/main/java/com/ahbin/log4j/test/Log4j4Test.java
+++ log/LOG4J_STUDY/src/main/java/com/ahbin/log4j/test/Log4j4Test.java
@@ -1,24 +1,24 @@
 package com.ahbin.log4j.test;
 
-import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
 import java.io.IOException;
 
 /**
- * 通过配置文件打印日志
+ * 日志级别输出测试
  */
-public class Log4j3Test {
+public class Log4j4Test {
 
     @Test
     public void test01() throws IOException {
         /*
-
+           日志级别输出测试
+           debug是我们在没有进行设置的情况下，默认的日志输出级别
          */
         //加载初始化配置
 //        BasicConfigurator.configure();
-        Logger logger = Logger.getLogger(Log4j3Test.class);
+        Logger logger = Logger.getLogger(Log4j4Test.class);
         logger.fatal("fatal 信息");
         logger.error("error 信息");
         logger.warn("warn 信息");
